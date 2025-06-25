371866552f80a9861c19b6865ff8b94c77b168f0
kesha <73757143+JustKesha@users.noreply.github.com>
2025-06-25 22:05:20 +0400

    Moved website files to /docs
    Explained in first related commit

--- website/scripts/next-preview.js
+++
@@ -1,7 +0,0 @@
-export function updateNextVideoThumb(player) {
-    const nextVideoId = player.queue[player.video.index + 1] || player.queue[0];
-    $('#next-preview')
-        .css('background-image', `url(https://i.ytimg.com/vi/${nextVideoId}/maxresdefault.jpg)`)
-        .off('click')
-        .on('click', () => player.next());
-}
