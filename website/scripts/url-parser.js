ee42d018842d06ffd8d9d490ada54673703fe1d9
kesha <73757143+JustKesha@users.noreply.github.com>
2025-06-25 18:08:35 +0400

    Added url parser for video ids

---
+++ website/scripts/url-parser.js
@@ -0,0 +1,23 @@
+import { splitByLength } from './utils.js';
+import { config } from './config.js';
+
+export function parseUrlActions(player, url) {
+    const searchParams = new URL(url).searchParams;
+
+    Array.from(searchParams).forEach(searchParam => {
+        const paramName = searchParam[0];
+        const paramValue = searchParam[1];
+
+        switch (paramName) {
+            case 'r':
+            case 'i':
+            case 'ids':
+                const ytVideoIDs = splitByLength(paramValue, config.YT_VIDEO_ID_LENGTH);
+
+                ytVideoIDs.forEach(ytVideoID => {
+                    player.queueUp(ytVideoID);
+                });
+                break;
+        }
+    })
+}
