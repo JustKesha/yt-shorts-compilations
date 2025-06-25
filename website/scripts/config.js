710acb21f58cdbf007d7dd095e4c759a29fb3595
kesha <73757143+JustKesha@users.noreply.github.com>
2025-06-25 22:38:26 +0400

    Added skip timeout, closes #22

--- website/scripts/config.js
+++ website/scripts/config.js
@@ -4,6 +4,7 @@ export const config = {
     YT_VIDEO_ID_LENGTH: 11,
     SHORTS_PLAYER_CONFIG: {
         autoskip: true,
+        skipdelay: 500,
     },
     AMBIENT_COLORS: {
         DEFAULT: "#ffffff80",
