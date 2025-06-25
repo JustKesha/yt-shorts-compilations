4dc485161e2fbf0f05140b0008ab9cd296d2b351
kesha <73757143+JustKesha@users.noreply.github.com>
2025-06-25 18:09:37 +0400

    Using new url parser in main

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,9 +1,11 @@
+import { config } from './config.js';
 import { ShortsPlayer } from './shorts-player.js';
 import { initYouTubeIFrameAPI } from './yt-iframe-api.js';
 import { initInputHandlers } from './input-handler.js';
-import { config } from './config.js';
+import { parseUrlActions } from './url-parser.js';
 
 const player = new ShortsPlayer(config.SHORTS_PLAYER_CONFIG);
 
 initYouTubeIFrameAPI(player);
 initInputHandlers(player);
+parseUrlActions(player, window.location.href);
