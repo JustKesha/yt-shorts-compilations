85992f78f2376f99389e6a90ede45e57f2f22a86
kesha <73757143+JustKesha@users.noreply.github.com>
2025-06-25 18:08:03 +0400

    Moved ShortsPlayer options into config

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,15 +1,9 @@
 import { ShortsPlayer } from './shorts-player.js';
-import { initInputHandlers } from './input-handler.js';
 import { initYouTubeIFrameAPI } from './yt-iframe-api.js';
+import { initInputHandlers } from './input-handler.js';
+import { config } from './config.js';
 
-const player = new ShortsPlayer({
-	queue: [
-        "dNKSxTYxB0E",
-        "Jn7Y0jeP8gY",
-        "DYeJtD8uXEA",
-    ],
-    autoskip: true,
-});
+const player = new ShortsPlayer(config.SHORTS_PLAYER_CONFIG);
 
 initYouTubeIFrameAPI(player);
 initInputHandlers(player);
