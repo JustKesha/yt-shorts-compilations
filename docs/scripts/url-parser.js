e9674508e444ff505b88aa03d57d8bfb5890637e
kesha <73757143+JustKesha@users.noreply.github.com>
2025-07-06 04:01:52 +0400

    Update progress bar / More responsive to que size

--- docs/scripts/url-parser.js
+++ docs/scripts/url-parser.js
@@ -19,20 +19,28 @@ export function parseUrlActions(player, url) {
                 });
 
                 // TODO Should instead be put in player class in case videos are added not via inital url
-                let progressOnhoverFlex = 1;
+                let progressOnhoverFlex = 1, progressActiveFlex = 1, progressGap ="4px";
 
                 // (TODO Replace with a switch case)
                 // (TODO Place all hardcoded values in config)
                 if (ytVideoIDs.length > 10) // Threshold
-                
+
                     if (ytVideoIDs.length < 25) {
-                        progressOnhoverFlex = 1.25;
-                    } else
-                    if (ytVideoIDs.length < 50) {
+                        progressActiveFlex = 1.5;
                         progressOnhoverFlex = 1.5;
                     } else
+                    if (ytVideoIDs.length < 50) {
+                        progressActiveFlex = 1.75;
                         progressOnhoverFlex = 1.75;
+                        progressGap = "2px";
+                    } else {
+                        progressActiveFlex = 2;
+                        progressOnhoverFlex = 2;
+                        progressGap = "2px";
+                    }
 
+                $(':root').css('--progress-gap', progressGap);
+                $(':root').css('--progress-active-flex', progressActiveFlex);
                 $(':root').css('--progress-onhover-flex', progressOnhoverFlex);
                 break;
             case 's':
